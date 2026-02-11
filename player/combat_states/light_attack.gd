60c8e437dc41801c5ef696e927e034b9ee5ff574
Alec Singer <sosenteam@gmail.com>
2026-02-11 10:22:12 -0800

    added 2d camera, fixed camera centering started ground tilemap

--- player/combat_states/light_attack.gd
+++ player/combat_states/light_attack.gd
@@ -6,7 +6,7 @@ func enter(previous_state_path: String, data := {}) -> void:
 	weapon = player.get_node("Weapon")
 	weapon.visible = true
 	swordAnimPlayer.play("light_attack")
-	weapon.rotation = player.position.angle_to_point(player.mouse_position)
+	weapon.rotation = Vector2(get_viewport().get_visible_rect().size.x/2,get_viewport().get_visible_rect().size.y/2).angle_to_point(player.mouse_position)
 func update(_delta: float) -> void:
 	if(!swordAnimPlayer.is_playing()):
 		finished.emit(IDLE_ATTACK)
